c33bc929f9509bcf7503d6412dfa161d155dbd2e
kif <kifff.tn@gmail.com>
2024-11-25 22:02:34 +0700

    mengubah file index

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello world</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem et ipsa sunt cumque modi perspiciatis. Accusantium ratione at sequi maxime ex quis quas aliquid sit, quod laborum provident autem molestiae fugiat praesentium consequuntur cumque illo aspernatur error dolorum consectetur a modi quia itaque. Fugit doloribus esse rem blanditiis asperiores dolorem.</p>
-</body>
-</html>
+<?php
+// Nama file CSV
+$file = 'data.csv';
+
+// Buka file CSV
+if (($handle = fopen($file, 'r')) !== false) {
+    // Loop untuk membaca setiap baris
+    while (($data = fgetcsv($handle, 1000, ',')) !== false) {
+        // Tampilkan data per baris (array)
+        print_r($data);
+    }
+    // Tutup file
+    fclose($handle);
+} else {
+    echo "Gagal membuka file.";
+}
