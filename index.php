a8b9baa9c0e753267d79a22a2105d23c7db7395d
teddyedwar29 <teddyedwar@gmail.com>
2024-11-25 21:54:08 +0700

    menambah p

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem et ipsa sunt cumque modi perspiciatis. Accusantium ratione at sequi maxime ex quis quas aliquid sit, quod laborum provident autem molestiae fugiat praesentium consequuntur cumque illo aspernatur error dolorum consectetur a modi quia itaque. Fugit doloribus esse rem blanditiis asperiores dolorem.</p>
 </body>
 </html>
